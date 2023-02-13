4fdae14803af5df437ec852f505b4692207ef0bc
Ramzan-iub <124768194+Ramzan-iub@users.noreply.github.com>
2023-02-13 10:25:10 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,54 @@
+#sting
+y="ramzan"
+print(y)
+
+
+#variable
+x= 5
+y= "asad"
+print(x,y)
+
+#list
+list=["uzma", "alia", "056"]
+print(list)
+
+#tuple 
+tuple = ("uzma", "alia", "056")
+print(tuple)
+
+#set
+set= {"alia", "uzma", "056"}
+print(set)
+
+#dictionaries
+dictio = {
+    "name": "ramzan",
+        "roll no": 56
+        }
+print(dictio)
+
+#if-else code 
+a= 50
+b= 40
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")   
+    
+#for loop
+fruits= ["pineapple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    
+#while loop
+i= 2
+while i < 7:
+    print(i)
+    if i == 3:
+        break
+    i+= 3
+    
+#function 
+def my_function():
+    print("i am student at IUB")     
+my_function()
